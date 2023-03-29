016d28d7136c7554d67c16ec4bbb08d76985ea94
Pattarapon <pattaraponknot@gmail.com>
2023-03-29 14:07:34 +0700

    complete function in controller and add email, unAvailableDates in model

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -1,36 +1,79 @@
-const Hotel = require('../models/Hotel');
+const Hotel = require("../models/Hotel");
 
 //@desc     Get all hotels
 //@route    GET /api/v1/hotels
 //@access   Public
 exports.getHotels = async (req, res, next) => {
-  res.status(200).json({ success: true, msg:"show all hotels"});
-}
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json({ success: true, count: hotels.length, data: hotels });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@desc     Get single hotel
 //@route    GET /api/v1/hotels/:id
 //@access   Public
 exports.getHotel = async (req, res, next) => {
-  res.status(200).json({ success: true, msg:`show hotel ${req.params.id}`});
-}
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    if (!hotel) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: hotel });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@desc     Create a hotel
 //@route    POST /api/v1/hotels
 //@access   Private
 exports.createHotel = async (req, res, next) => {
-  res.status(200).json({ success: true });
-}
+  try {
+    const hotel = await Hotel.create(req.body);
+    res.status(200).json({ success: true, data: hotel });
+  } catch (err) {
+    if (err.code && err.code === 11000) {
+      return res
+        .status(400)
+        .json({ success: false, message: "This email has already taken" });
+    }
+  }
+};
 
 //@desc     Update single hotel
 //@route    PUT /api/v1/hotels/:id
 //@access   Private
 exports.updateHotel = async (req, res, next) => {
-  res.status(200).json({ success: true });
-}
+  try {
+    const hotel = await Hotel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!hotel) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(200).json({ success: true, data: hotel });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@desc     Delete single hotel
 //@route    DELETE /api/v1/hotels/:id
 //@access   Private
 exports.deleteHotel = async (req, res, next) => {
-  res.status(200).json({ success: true });
-}
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    if (!hotel) {
+      return res.status(400).json({ success: false });
+    }
+    hotel.deleteOne();
+    res.status(200).json({ success: true, data: {} });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
--- models/Hotel.js
+++ models/Hotel.js
@@ -1,22 +1,44 @@
 const mongoose = require("mongoose");
 
-const HotelSchema = new mongoose.Schema({
-  name:{
-    type: String,
-    require: [true, 'Please add a name'],
-    unique: true,
-    trim: true,
+const HotelSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "Please add a name"],
+      unique: true,
+      trim: true,
+    },
+    address: {
+      type: String,
+      require: [true, "Please add an address"],
+    },
+    telNo: {
+      type: String,
+      required: [true, "Please add a telephone number"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please add a valid email",
+      ],
+    },
+    unAvailableDates:{
+      type: [String],
+    }
   },
-  address:{
-    type: String,
-    require: [true, 'Please add an address']
-  },
-  telNo: {
-    type: String
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
-},{
-  toJSON: {virtuals:true},
-  toObject: {virtuals:true}
+);
+
+HotelSchema.pre('remove', async function(next){
+  // console.log(`Appointment being removed from hospital ${this._id}`);
+  // await this.model('Appointment').deleteMany({hospital:this._id});
+  next();
 })
 
 module.exports = mongoose.model("Hotel", HotelSchema);
